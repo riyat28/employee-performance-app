d1173f62793fc2773fdfe63f3d429e8b181c7c7f
Riya Tambe <riya.tambe28@gmail.com>
2026-05-25 13:42:35 +0530

    Implement Database Config with Singleton Behaviour

---
+++ employee-performance-app/src/main/java/com/enterprise/config/DatabaseConfig.java
@@ -0,0 +1,41 @@
+import java.util.Properties;
+
+
+import java.io.IOException;
+import java.lang.RuntimeException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.InputStream;
+public class DatabaseConfig {
+    private static final Properties properties= new Properties();
+    // Static block runs ONCE when the class is loaded into memory
+    static{
+    try(InputStream input= DatabaseConfig.class.getClassLoader().getResourceAsStream("db.properties")){
+       if(input==null){
+        throw new RuntimeException("Unable to find db.properties in resources folder");
+       }
+       // Load the properties file
+       properties.load(input);
+       // Explicitly register the MySQL driver class (Good practice in core JDBC)
+       Class.forName("com.mysql.cj.jdbc.Driver");
+
+    }
+    catch(IOException e){
+        throw new RuntimeException("Failed to load database configuration properties",e);
+    }
+    catch(ClassNotFoundException e){
+        throw new RuntimeException("MySQL JDBC Driver not found in classpath",e);
+    }
+}
+// Private constructor to prevent instantiation
+private DatabaseConfig(){};
+public static Connection getConnection() throws SQLException{
+   return DriverManager.getConnection(
+    properties.getProperty("db.url"),
+    properties.getProperty("db.username"),
+    properties.getProperty("db.password")
+   );
+}
+
+}
